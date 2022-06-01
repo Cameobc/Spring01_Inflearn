ef7b93c42bc070fddfa2b606ebc2571718649a47
Cameobc <dkqtk@naver.com>
2022-06-01 20:18:48 +0900

    0601 DL. ObjectProvider 를 사용함

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,7 +1,9 @@
 package hello.core.scope;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -13,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class SingletonWithPrototypeTest1 {
 
     @Test
-    void prototypeFind(){
+    void prototypeFind() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
 
         PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
@@ -26,7 +28,7 @@ public class SingletonWithPrototypeTest1 {
     }
 
     @Test
-    void singletonClientUsePrototype(){
+    void singletonClientUsePrototype() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
         ClientBean clientBean1 = ac.getBean(ClientBean.class);
         int count1 = clientBean1.logic();
@@ -34,57 +36,46 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
-    @Scope("singleton")
-    static class ClientBean2{
-        private final PrototypeBean prototypeBean;  // 생성 시점에 주입_01
-
-        public ClientBean2(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
-
-        public int logic(){
-            prototypeBean.addCount();
-            return prototypeBean.getCount();
-        }
-    }
 
     @Scope("singleton")
-    static class ClientBean{
-        private final PrototypeBean prototypeBean;  // 생성 시점에 주입_02
+    static class ClientBean {
 
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        @Autowired
+        // Factory, Provieder 둘 다 사용가능함. 모두 스프링에 의존적임
+        // private ObjectFactory<PrototypeBean> prototypeBeanProvider;
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
-        public int logic(){
+
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
     }
 
     @Scope("prototype")
-    static class PrototypeBean{
+    static class PrototypeBean {
         private int count = 0;
 
-        public void addCount(){
+        public void addCount() {
             count++;
         }
 
-        public int getCount(){
+        public int getCount() {
             return count;
         }
 
         @PostConstruct
-        public void init(){
+        public void init() {
             System.out.println("PrototypeBean.init " + this);
 
         }
 
         @PreDestroy
-        public void destroy(){
+        public void destroy() {
             System.out.println("PrototypeBean.destroy");
         }
     }
