e0ca72b388082bd740d0995a2eefc4a9affa5dcd
Cameobc <dkqtk@naver.com>
2022-06-01 10:17:08 +0900

    220601_롬북

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,37 +3,23 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+// final 이 붙은 것을 생성자로 만들어준다.
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
-
-
-    @Autowired
-    public void setMemberRepository(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
-    @Autowired
-    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-        this.discountPolicy = discountPolicy;
-    }
-
-    @Autowired
+/*
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        System.out.println("memberRepository = " + memberRepository);
-        System.out.println("discountPolicy = " + discountPolicy);
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
-    }
-    //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-
+    }*/
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -43,8 +29,4 @@ public class OrderServiceImpl implements OrderService {
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 
-    //test
-/*    public static MemberRepository getMemberRepository() {
-        return memberRepository;
-    }*/
 }
